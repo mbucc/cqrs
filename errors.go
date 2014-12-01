6b59f4c74ab57b45dc5f76bc4635573b7027aaf7
Mark Bucciarelli <mkbucc@gmail.com>
2014-11-30 21:17:45 -0500

    column -t is handy sometimes.

--- errors.go
+++ errors.go
@@ -7,26 +7,16 @@ Author: Mark Bucciarelli <mkbucc@gmail.com>
 */
 package main
 
-type TabNotOpen int
+type  TabNotOpen            int
+type  DrinksNotOutstanding  int
+type  FoodNotOutstanding    int
+type  FoodNotPrepared       int
+type  MustPayEnough         int
+type  TabHasUnservedItems   int
 
-func (i TabNotOpen) Error() string { return "TabNotOpen" }
-
-type DrinksNotOutstanding int
-
-func (i DrinksNotOutstanding) Error() string { return "DrinksNotOutstanding" }
-
-type FoodNotOutstanding int
-
-func (i FoodNotOutstanding) Error() string { return "FoodNotOutstanding" }
-
-type FoodNotPrepared int
-
-func (i FoodNotPrepared) Error() string { return "FoodNotPrepared" }
-
-type MustPayEnough int
-
-func (i MustPayEnough) Error() string { return "MustPayEnough" }
-
-type TabHasUnservedItems int
-
-func (i TabHasUnservedItems) Error() string { return "TabHasUnservedItems" }
+func  (i TabNotOpen)            Error()  string  { return  "TabNotOpen"           }
+func  (i DrinksNotOutstanding)  Error()  string  { return  "DrinksNotOutstanding" }
+func  (i FoodNotOutstanding)    Error()  string  { return  "FoodNotOutstanding"   }
+func  (i FoodNotPrepared)       Error()  string  { return  "FoodNotPrepared"      }
+func  (i MustPayEnough)         Error()  string  { return  "MustPayEnough"        }
+func  (i TabHasUnservedItems)   Error()  string  { return  "TabHasUnservedItems"  }
