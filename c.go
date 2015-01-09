96d4de02583649688e22aba49cb7c4ebddd1f5e9
Mark Bucciarelli <mkbucc@gmail.com>
2015-01-08 22:39:35 -0500

    Wordsmithing comments, no logic change.

--- c.go
+++ c.go
@@ -1,26 +1,26 @@
 // A Go version of Edument's (http://cqrs.nu/) CQRS sample application.
 // CQRS stands for command/query responsibility separation.
+// I wanted to learn about command/query separation.
 package main
 
 // A TabId represents a tab, or bill, at a cafe table.
 type TabId int
 
 // A command is an action
-// that can either be accepted or rejected
-// A command is always associated with a tab;
+// that can be accepted or rejected
 // for example, MoreDrinksWench!
 // might be a command in a medieval
 // misogynistic kind of cafe.
 type Command interface{}
 
-// An event is something that actually happens.
-// and are triggered by commands.
-// For example, FaceSlapped.
+// An event is something that happened
+// as a result of a command;
+// for example, FaceSlapped.
 type Event interface{}
 
 // Command handlers are responsible
 // for validating commands,
-// both in as a stand-alone type
+// both as a stand-alone set of data
 // as well as in the context
 // of the Command's aggregate (I know,
 // lots of undefined terms here ...
