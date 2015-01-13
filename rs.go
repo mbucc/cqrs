3be404eb1d6aab96ba17d1a72486fdd032648f4c
Mark Bucciarelli <mkbucc@gmail.com>
2015-01-12 22:30:12 -0500

    Add event handling "test"
    In quotes, because it passes even though handlers are stubbed.
    Basically an excuse to write an event handler that uses
    the OSX "say" command ...
    
           man say | say -v Hysterical
    
    for example.

--- rs.go
+++ rs.go
@@ -18,12 +18,19 @@ type CommandHandler interface {
 	handle(c Command) (e []Event, err error)
 }
 
+type EventListener interface {
+	apply(e Event) error
+}
+
 // Maps a command to its handler.
 type HandlerRegistry map[reflect.Type]CommandHandler
+
+type ListenerRegistry map[reflect.Type][]EventListener
 
 // Registers event and command listeners.  Dispatches commands.
 type messageDispatcher struct {
 	registry HandlerRegistry
+	listeners ListenerRegistry
 }
 
 func (md *messageDispatcher) SendCommand(c Command) ([]Event, error) {
@@ -34,7 +41,30 @@ func (md *messageDispatcher) SendCommand(c Command) ([]Event, error) {
 	return nil, errors.New(fmt.Sprint("No handler registered for command ", t))
 }
 
-func NewMessageDispatcher(hr HandlerRegistry) (*messageDispatcher, error) {
+// Since events represent a thing that actually happened,
+// a fact, having an event listener return an error
+// is probably not the right thing to do.
+// While errors can certainly occor,
+// for example, email server or database is down
+// or the file system is full,
+// a better approach would be to stick the events
+// in a durable queue so when the error condition clears
+// the listener can successfully do it's thing.
+func (md *messageDispatcher) PublishEvent(e Event) error {
+	var err error;
+	t := reflect.TypeOf(e)
+	if a, ok := md.listeners[t]; ok {
+		for _, listener := range a {
+			err = listener.apply(e)
+			if err != nil {
+				return err;
+			}
+		}
+	}
+	return nil
+}
+
+func NewMessageDispatcher(hr HandlerRegistry, lr ListenerRegistry) (*messageDispatcher, error) {
 	m := make(HandlerRegistry, len(hr))
 	for commandtype, handler := range hr {
 		m[commandtype] = handler
--- rs_test.go
+++ rs_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"reflect"
 	"testing"
+	"log"
 )
 
 type ShoutOut struct {
@@ -33,6 +34,20 @@ func (eh *EchoHandler) handle(c Command) (a []Event, err error) {
 	return a, nil
 }
 
+type SayItHysterically struct{}
+
+func (h *SayItHysterically) apply(e Event) error {
+	log.Print("SayItHysterically stub")
+	return nil
+}
+
+type SayItBubbly struct{}
+
+func (h *SayItBubbly) apply(e Event) error {
+	log.Printf("SayItBubbly stub")
+	return nil
+}
+
 func TestHandledCommandReturnsEvents(t *testing.T) {
 
 	Convey("Given a shout out and a shout out handler", t, func() {
@@ -57,7 +72,7 @@ func TestSendCommand(t *testing.T) {
 	Convey("Given a dispatcher that sends ShoutOuts to an EchoHandler", t, func() {
 		registry := HandlerRegistry{
 			reflect.TypeOf(new(ShoutOut)): new(EchoHandler)}
-		md, err := NewMessageDispatcher(registry)
+		md, err := NewMessageDispatcher(registry, nil)
 		So(err, ShouldEqual, nil)
 
 		Convey("Sending a ShoutOut should return a single HeardIt event", func() {
@@ -65,6 +80,26 @@ func TestSendCommand(t *testing.T) {
 			So(err, ShouldEqual, nil)
 			So(len(events), ShouldEqual, 1)
 			So(events[0].(*HeardIt).Heard, ShouldEqual, "hello world")
+		})
+	})
+}
+
+func TestPublishEvent(t *testing.T) {
+
+	Convey("Given an echo handler and a couple SayIt listeners", t, func() {
+		listeners := ListenerRegistry{
+			reflect.TypeOf(new(HeardIt)):
+				[]EventListener{new(SayItHysterically), new(SayItBubbly)}}
+		registry := HandlerRegistry{
+				reflect.TypeOf(new(ShoutOut)): new(EchoHandler)}
+		md, err := NewMessageDispatcher(registry, listeners)
+		So(err, ShouldEqual, nil)
+
+		Convey("A ShoutOut should make noise", func() {
+			events, err := md.SendCommand(&ShoutOut{1, "hello humanoid"})
+			So(err, ShouldEqual, nil)
+			So(len(events), ShouldEqual, 1)
+			md.PublishEvent(events[0])
 		})
 	})
 }
