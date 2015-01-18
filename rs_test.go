b3de8a7cf0a64e1a3653488423bf79842c935082
Mark Bucciarelli <mkbucc@gmail.com>
2015-01-17 21:00:34 -0500

    Zap leading tab that got inserted on each line.
    Fun with learning the sam editor.  :)

--- rs_test.go
+++ rs_test.go
@@ -1,123 +1,123 @@
-	package main
+package main
 
-	import (
-		"log"
-		"reflect"
-		"testing"
-		. "github.com/smartystreets/goconvey/convey"
-	)
+import (
+	"log"
+	"reflect"
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
 
-	var testChannel = make(chan string)
+var testChannel = make(chan string)
 
-	type ShoutOut struct {
-		id      AggregateId
-		Comment string
-	}
+type ShoutOut struct {
+	id      AggregateId
+	Comment string
+}
 
-	func (c *ShoutOut) Id() AggregateId {
-		return c.id
-	}
+func (c *ShoutOut) Id() AggregateId {
+	return c.id
+}
 
-	type HeardIt struct {
-		id    AggregateId
-		Heard string
-	}
+type HeardIt struct {
+	id    AggregateId
+	Heard string
+}
 
-	func (e *HeardIt) Id() AggregateId {
-		return e.id
-	}
+func (e *HeardIt) Id() AggregateId {
+	return e.id
+}
 
-	type EchoHandler struct{}
+type EchoHandler struct{}
 
-	func (eh *EchoHandler) handle(c Command) (a []Event, err error) {
-		a = make([]Event, 1)
-		c1 := c.(*ShoutOut)
-		a[0] = &HeardIt{c1.Id(), c1.Comment}
-		return a, nil
-	}
+func (eh *EchoHandler) handle(c Command) (a []Event, err error) {
+	a = make([]Event, 1)
+	c1 := c.(*ShoutOut)
+	a[0] = &HeardIt{c1.Id(), c1.Comment}
+	return a, nil
+}
 
-	type WriteToChannel struct{}
+type WriteToChannel struct{}
 
-	func (h *WriteToChannel) apply(e Event) error {
-		testChannel <- e.(*HeardIt).Heard
-		return nil
-	}
+func (h *WriteToChannel) apply(e Event) error {
+	testChannel <- e.(*HeardIt).Heard
+	return nil
+}
 
 
-	func TestHandledCommandReturnsEvents(t *testing.T) {
+func TestHandledCommandReturnsEvents(t *testing.T) {
 
-		Convey("Given a shout out and a shout out handler", t, func() {
+	Convey("Given a shout out and a shout out handler", t, func() {
 
-			shout := ShoutOut{1, "ab"}
-			h := EchoHandler{}
+		shout := ShoutOut{1, "ab"}
+		h := EchoHandler{}
 
-			Convey("When the shout out is handled", func() {
+		Convey("When the shout out is handled", func() {
 
-				rval, _ := h.handle(&shout)
+			rval, _ := h.handle(&shout)
 
-				Convey("It should return one event", func() {
+			Convey("It should return one event", func() {
 
-					So(len(rval), ShouldEqual, 1)
-				})
+				So(len(rval), ShouldEqual, 1)
 			})
 		})
+	})
+}
+
+func TestSendCommand(t *testing.T) {
+
+	Convey("Given a dispatcher that sends ShoutOuts to an EchoHandler", t, func() {
+		registry := HandlerRegistry{
+			reflect.TypeOf(new(ShoutOut)): new(EchoHandler)}
+		md, err := NewMessageDispatcher(registry, nil)
+		So(err, ShouldEqual, nil)
+
+		Convey("Sending a ShoutOut should return a single HeardIt event", func() {
+			events, err := md.SendCommand(&ShoutOut{1, "hello world"})
+			So(err, ShouldEqual, nil)
+			So(len(events), ShouldEqual, 1)
+			So(events[0].(*HeardIt).Heard, ShouldEqual, "hello world")
+		})
+	})
+}
+
+func TestPublishEvent(t *testing.T) {
+	log.Println("starting TestPublishEvent")
+
+	var err0 error
+	if err0 != nil {
+		log.Panicf("can't open temp file");
 	}
 
-	func TestSendCommand(t *testing.T) {
-
-		Convey("Given a dispatcher that sends ShoutOuts to an EchoHandler", t, func() {
-			registry := HandlerRegistry{
+	Convey("Given an echo handler and a couple SayIt listeners", t, func() {
+		listeners := ListenerRegistry{
+			reflect.TypeOf(new(HeardIt)):
+				[]EventListener{new(WriteToChannel), new(WriteToChannel)}}
+		registry := HandlerRegistry{
 				reflect.TypeOf(new(ShoutOut)): new(EchoHandler)}
-			md, err := NewMessageDispatcher(registry, nil)
+		md, err := NewMessageDispatcher(registry, listeners)
+		So(err, ShouldEqual, nil)
+
+		Convey("A ShoutOut should make noise", func() {
+			events, err := md.SendCommand(&ShoutOut{1, "hello humanoid"})
 			So(err, ShouldEqual, nil)
-
-			Convey("Sending a ShoutOut should return a single HeardIt event", func() {
-				events, err := md.SendCommand(&ShoutOut{1, "hello world"})
-				So(err, ShouldEqual, nil)
-				So(len(events), ShouldEqual, 1)
-				So(events[0].(*HeardIt).Heard, ShouldEqual, "hello world")
-			})
-		})
-	}
-
-	func TestPublishEvent(t *testing.T) {
-		log.Println("starting TestPublishEvent")
-
-		var err0 error
-		if err0 != nil {
-			log.Panicf("can't open temp file");
-		}
-
-		Convey("Given an echo handler and a couple SayIt listeners", t, func() {
-			listeners := ListenerRegistry{
-				reflect.TypeOf(new(HeardIt)):
-					[]EventListener{new(WriteToChannel), new(WriteToChannel)}}
-			registry := HandlerRegistry{
-					reflect.TypeOf(new(ShoutOut)): new(EchoHandler)}
-			md, err := NewMessageDispatcher(registry, listeners)
-			So(err, ShouldEqual, nil)
-
-			Convey("A ShoutOut should make noise", func() {
-				events, err := md.SendCommand(&ShoutOut{1, "hello humanoid"})
-				So(err, ShouldEqual, nil)
-				So(len(events), ShouldEqual, 1)
-				go func() {
-					Convey("Should be able to publish events", t, func() {
-						err := md.PublishEvent(events[0])
-						So(err, ShouldEqual, nil)
-					})
-					close(testChannel)
-				}()
-				n := 0
-				for {
-					msg, channelOpen := <- testChannel
-					if ! channelOpen {
-						break
-					}
-					n = n + 1
-					So(msg, ShouldEqual, "hello humanoid")
+			So(len(events), ShouldEqual, 1)
+			go func() {
+				Convey("Should be able to publish events", t, func() {
+					err := md.PublishEvent(events[0])
+					So(err, ShouldEqual, nil)
+				})
+				close(testChannel)
+			}()
+			n := 0
+			for {
+				msg, channelOpen := <- testChannel
+				if ! channelOpen {
+					break
 				}
-				So(n, ShouldEqual, 2)
-			})
+				n = n + 1
+				So(msg, ShouldEqual, "hello humanoid")
+			}
+			So(n, ShouldEqual, 2)
 		})
-	}
+	})
+}
